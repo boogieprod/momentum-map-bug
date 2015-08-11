6f238b5ce7c9048a050fe89daec0d5396255adf7
boogieprod <frederick.bouchard88@gmail.com>
2015-08-11 17:34:04 -0400

    edited code to match meteorpad reproduction app
    http://meteorpad.com/pad/7sA3LWFthgiBbsWEs/Copy%20of%20setTimeout%20%281%29

--- mapbug-route.js
+++ mapbug-route.js
@@ -17,9 +17,7 @@ if (Meteor.isClient) {
 
   Template.map.helpers({
     exampleMapOptions: function() {
-      // Make sure the maps API has loaded
       if (GoogleMaps.loaded()) {
-        // Map initialization options
         return {
           center: new google.maps.LatLng(-37.8136, 144.9631),
           zoom: 8
